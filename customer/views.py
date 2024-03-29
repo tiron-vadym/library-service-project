82eb81ee30e8c3c00ad5f31c651cb4e9acb0bfd7
Vadym Tiron <tironvadim1@gmail.com>
2024-03-29 17:32:27 +0200

    fix flake8

--- customer/views.py
+++ customer/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
 from customer.serializers import UserSerializer
