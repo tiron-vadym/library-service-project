2467c126d105b15e6c5f17decc01a03546f62051
Vadym Tiron <tironvadim1@gmail.com>
2024-04-05 22:26:36 +0300

    update settings.py

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -40,12 +40,15 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_spectacular",
+    "debug_toolbar",
     "books_service",
     "customer",
+    "borrowings_service",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
