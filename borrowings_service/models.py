b8c8bcd7f1bfd8b2f230c0bdf0d2eef6291e4722
Vadym Tiron <tironvadim1@gmail.com>
2024-04-12 16:15:36 +0300

    fix flake8 in models.

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -9,8 +9,16 @@ class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowingb")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowingu")
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowingb"
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="borrowingu"
+    )
 
     def __str__(self):
         return f"Borrowing: {self.book} by User: {self.user}"
