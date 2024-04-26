afe16af0804affea0d0bc7e85e297322fac8a84f
Vadym Tiron <tironvadim1@gmail.com>
2024-04-26 21:17:40 +0300

    fix flake8 and upgrade requirements.txt

--- utilities/bot.py
+++ utilities/bot.py
@@ -7,6 +7,7 @@ def send_message(text):
     token = os.environ.get("BOT_TOKEN")
     chat_id = os.environ.get("CHAT_ID")
     url = (
-        f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}"
+        f"https://api.telegram.org/bot{token}/"
+        f"sendMessage?chat_id={chat_id}&text={text}"
     )
     requests.post(url)
