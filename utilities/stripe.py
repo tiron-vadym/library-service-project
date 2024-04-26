6054519dfd46e1fa2bb0013cbfe0567b7ac904c6
Vadym Tiron <tironvadim1@gmail.com>
2024-04-26 21:12:23 +0300

    add functional for working with stripe sessions

---
+++ utilities/stripe.py
@@ -0,0 +1,86 @@
+import stripe
+from django.conf import settings
+from django.http import request
+from django.urls import reverse
+
+
+from borrowings_service.models import Borrowing
+from payments_service.models import Payment
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def calculate_total_amount(borrowing: Borrowing) -> int:
+    using_days = (borrowing.expected_return_date - borrowing.borrow_date).days
+    daily_fee_cents = borrowing.book.daily_fee * 100
+    looking_price = int(daily_fee_cents) * using_days
+
+    days_overdue = (
+            borrowing.actual_return_date - borrowing.expected_return_date
+    ).days
+    fine_amount = int((days_overdue * borrowing.book.daily_fee * 2) * 100)
+    total = fine_amount + looking_price
+
+    return stripe.Price.create(
+        product="prod_Pz5uWC2BXMLDzx",
+        unit_amount=total,
+        currency="usd",
+    )
+
+
+def session(price):
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                "price": price,
+                "quantity": 1,
+            },
+        ],
+        mode="payment",
+        success_url=request.build_absolute_uri(
+            reverse("payments_service:success")
+        )
+        + f"?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url=request.build_absolute_uri(
+            reverse("payments_service:cancel")
+        )
+        + f"?session_id={CHECKOUT_SESSION_ID}",
+    )
+    return checkout_session
+
+
+def stripe_helper(borrowing):
+    price = calculate_total_amount(borrowing)
+    checkout_session = session(price)
+    payment = Payment.objects.create(
+        borrowing=borrowing,
+        session_url=checkout_session.url,
+        session_id=checkout_session.id,
+        money_to_pay=price["unit_amount"] / 100,
+    )
+    return payment
+
+
+def pay_fine(borrowing: Borrowing) -> int:
+    if borrowing.actual_return_date > borrowing.expected_return_date:
+        days_overdue = (
+            borrowing.actual_return_date - borrowing.expected_return_date
+        ).days
+        fine_amount = int((days_overdue * borrowing.book.daily_fee * 2) * 100)
+
+        price = stripe.Price.create(
+            product="prod_Pz5uWC2BXMLDzx",
+            unit_amount=fine_amount,
+            currency="usd",
+        )
+
+        checkout_session = session(price)
+        payment = Payment.objects.create(
+            status=Payment.PaymentStatus.PAID,
+            type=Payment.PaymentType.FINE,
+            borrowing=borrowing,
+            session_url=checkout_session.url,
+            session_id=checkout_session.id,
+            money_to_pay=fine_amount / 100,
+        )
+        return payment
